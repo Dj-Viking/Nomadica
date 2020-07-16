eb51fe449b9ff0578e04ad97476be750bf54b491
Daniel Monterrosa <65088117+Dannymont94@users.noreply.github.com>
2020-07-16 00:26:06 -0400

    wrapped functions in script.js within formSubmitHandler function. reformatted file and clarified variable and function names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,143 +1,90 @@
-//country-name-salary-test-branch
-//July, 15 2020
-
-
-//  Anders Ackerman test code for the api calls of the api Teleport
+//  Sickest group of all time code for Teleport API calls
 //      * currently used to get the countryCode iso_alpha2 format i.e. "US" based on user prompt input of the country name "United States" or "united states"
-//      * gets country salary Code from the country name entered
-//      * gets the salary of the web developers in that country
-//          - the salary shown is based on the currency of that country
-//      * a function getCountryList() is declared at the bottom here to show the country list in a for loop console logged
-//      * the function apiTestFetch() is just testing the api at the top of the api tree
+//      * gets country currency code from the country name entered
+//      * gets median annual salary (USD) of web developers in that country
+//      * unused function getCountryList() is declared at the bottom here to show the country list in a for loop console logged
 
-let countryName = window.prompt("search a city you want to check the salaries of that country");
+const userFormEl = document.querySelector("#user-form");
+const userInputEl = document.querySelector("#user-input");
 
-//this will hold the value of the string a user enters
-//this function is in a different JS file!! must place it above script.js!!
-let countryCode = getCountryCodeOrName(countryName)
+function formSubmitHandler() {
 
-getCountryCurrencyCode(countryCode[1]);
+    // this will hold the value of the user's search
+    let searchTerm = userInputEl.value;
 
-getCountrySalaryPercentiles(countryCode[1]);
-//index [1] is the returned string value of the country name entered only if the user has entered a country name in the this format "United States" or "united states"
-//MUST have entered the country name and not the code
-//if the country code was entered, the country code will be index [0]
-console.log(countryCode[1]);
+    // this function is in a different JS file!! must place it above script.js!!
+    // this function returns an array with country code and country name index flipped depending on search term
+    let countryCode = getCountryCodeOrName(searchTerm);
 
+    // if the country code was searched, the country code will be index [0]. if country name was searched, the country code will be index [1]
+    countryCode = searchTerm.length === 2 ? countryCode[0] : countryCode[1];
+    console.log(`country code: ${countryCode}`);
 
-//get the salary code from the country code
-function getCountryCurrencyCode(countryCode){
+    let currencyCode = getCurrencyCode(countryCode);
+    // this console.logs undefined since getCurrencyCode is an async function. not sure how to fix.
+    console.log(`currency code: ${currencyCode}`);
+    
+    let medianSalary = getMedianSalary(countryCode);
+    // this console.logs undefined since getMedianSalary is an async function. not sure how to fix.
+    console.log(`median salary: ${medianSalary}`);
+}
 
-    fetch(
-        `https://api.teleport.org/api/countries/iso_alpha2:${countryCode}/`
-        )
-        .then(function(response){
+// get the currency code from the country code for conversion API
+function getCurrencyCode(countryCode){
+    fetch( `https://api.teleport.org/api/countries/iso_alpha2:${countryCode}/` )
+        .then( (response) => response.json() )
+        .then( (data) => {
+            let currencyCode = data.currency_code;
+            // get currency code
+            console.log(`country salary code: ${currencyCode}`);
+            return currencyCode;
+        });
+}
+
+function getMedianSalary(countryCode){
+    fetch( `https://api.teleport.org/api/countries/iso_alpha2:${countryCode}/salaries/` )
+        .then( (response) => response.json() )
+        .then( ({salaries}) => {
+            console.log("salary info for various jobs in the searched country:");
+            console.log(salaries);
+
+            for (let i = 0; i < salaries.length; i++) {
+                if (salaries[i].job.id == "WEB-DEVELOPER") {
+                    let medianSalary = salaries[i].salary_percentiles.percentile_50
+                    console.log(`median salary for web developers in the searched country: ${medianSalary}`);
+                    return medianSalary;
+                }
+            }
+        });
+}
+
+// this function is just going to display all the countries in the console
+// each country will have links that need to be fetched to obtain
+// this function is currently not being used
+function getCountryList(){
+    // search for country salaries based on country name
+    fetch( `https://api.teleport.org/api/countries/` )
+        .then( (response) => {
+            console.log("here is the first response from the country name api call");
+            console.log(response);
             return response.json();
         })
-        .then(function(response2){
-            console.log(response2)
-            let salaryCode = response2.currency_code;
-            //checking to see if we are getting the salary code
-            //example japans currency the yen is code JPY
-            console.log("displaying country salary code");
-            console.log(salaryCode);
+        .then( (data) => {
+            console.log("here is the second response from the country name api call");
+            console.log(data);
 
-        });
-}
-
-
-function getCountrySalaryPercentiles(code){
-
-
-    fetch(
-        `https://api.teleport.org/api/countries/iso_alpha2:${code}/salaries/`
-        ).then(function(response){
-            return response.json()
-        }).then(function(response2){
-            console.log("here are the salaries of jobs in the searched country")
-            console.log(response2);
-
-            console.log("here is the salary range of Web Developers in the searched country")
-            console.log(response2.salaries[51])
-        });
-}
-
-
-// apiTestFetch();
-// getCountryList();
-
-
-//my first testing of the api beginning point, this is by no means
-//  an end point api fetch
-function apiTestFetch(){
-    fetch(
-        // `https://api.openaq.org/v1/cities?country=${countryName}`
-        // "https://restcountries.eu/rest/v2/all"
-        `https://api.teleport.org/api/`
-    )
-    .then(function(response) {
-        console.log("test first returned promise")
-        
-        console.log(response);
-        return response.json();  
-    })
-    .then(function(response2){
-        console.log("returned response.json() in second promise")
-        
-        console.log(response2);
-
-        console.log("fetching country list object")
-        fetch(
-            response2._links["country:list"].href
-        )
-        .then(function(responseCountryList){
-            console.log("here is the second fetch first promise with the country list");
-            console.log(responseCountryList);
-            return responseCountryList.json();
-        })
-        .then(function(responseCountryList2){
-            console.log("here is the second fetch second promise with the countrylist json");
-            console.log(responseCountryList2);
-        })
-
-
-    });
-    
-}
-
-//this function is just going to display all the countries in the console
-//each country will have links that need to be fetched to obtain
-function getCountryList(){
-    //search for country salaries based on country name
-    fetch(
-        `https://api.teleport.org/api/countries/`
-    )
-    .then(function(response){
-        console.log("here is the first response from the country name api call");
-        console.log(response);
-        return response.json();
-    })
-    .then(function(response2){
-        console.log("here is the second response from the country name api call");
-        console.log(response2);
-
-        console.log("below displaying the json objects for each country..trying to get salaries from this")
-        //displaying all the countries just as a test of this loop
-        for (let i = 0; i < response2._links["country:items"].length; i++){
-
-            fetch(
-                response2._links["country:items"][i].href
-                )
-                .then(function(responseCountryApiHref){
-                    return responseCountryApiHref.json();
-                })
-                .then(function(responseCountryApiHref2){
-                    console.log(responseCountryApiHref2);
-                    
-                })    
+            console.log("below displaying the json objects for each country..trying to get salaries from this")
+            // displaying all the countries just as a test of this loop
+            for (let i = 0; i < data._links["country:items"].length; i++){
+                fetch( data._links["country:items"][i].href )
+                    .then( (response) => {
+                        return response.json();
+                    })
+                    .then( (data) => {
+                        console.log(data);
+                    })
             }
-        })
-    }
-   
+        });
+}
 
-    
+userFormEl.addEventListener("submit", formSubmitHandler);
